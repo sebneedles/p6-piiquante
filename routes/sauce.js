f775d9184ad99735f6f4f4ca5c6832ea36179096
sebneedles <sebneedles@gmail.com>
2022-09-20 11:43:00 +0200

    first commit

---
+++ routes/sauce.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const sauceCtrl = require('../controllers/sauce');
+
+router.post('/', sauceCtrl.createSauce);
+router.put('/:id', sauceCtrl.modifySauce);
+router.delete('/:id', sauceCtrl.deleteSauce);
+router.get('/:id', sauceCtrl.getOneSauce);
+router.get('/', sauceCtrl.getAllSauces);
+
+
+
+module.exports = router;
